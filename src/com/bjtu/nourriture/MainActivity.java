58371a54593797aeaa2fb6ee9cac7d4b781f2b09
zhaiyuan <zhacai925@gmail.com>
2014-12-25 16:55:17 +0800

    hihihihi

--- src/com/bjtu/nourriture/MainActivity.java
+++ src/com/bjtu/nourriture/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//hihihi
 public class MainActivity extends Activity {
 
 	@Override
