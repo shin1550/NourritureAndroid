0b5e8548411b799754c90d8affe493780133f7c6
MengJaeSuk <MengJaeSul@gmail.com>
2014-12-27 17:33:59 +0800

    图片显示出来了，贼开森

--- src/com/bjtu/nourriture/recipe/ListRecipeActivity.java
+++ src/com/bjtu/nourriture/recipe/ListRecipeActivity.java
@@ -1,6 +1,7 @@
 package com.bjtu.nourriture.recipe;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
@@ -15,21 +16,48 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bjtu.nourriture.R;
-import com.bjtu.nourriture.recipe.adapter.ListRecipeAdapter;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 public class ListRecipeActivity extends Activity{
 
+	DisplayImageOptions options;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		options = new DisplayImageOptions.Builder()
+		.showImageOnLoading(R.drawable.ic_launcher)
+		.showImageForEmptyUri(R.drawable.ic_launcher)
+		.showImageOnFail(R.drawable.ic_launcher)
+		.cacheInMemory(true)
+		.cacheOnDisk(true)
+		.considerExifParams(true)
+		.displayer(new RoundedBitmapDisplayer(20))
+		.build();
+		
 		setContentView(R.layout.activity_recipe_list_all);
 		TextView testView = (TextView)findViewById(R.id.textView1);
+		
 		String recipeRecult = getRecipeList();
 		try {
 			JSONObject jsonObject = new JSONObject(recipeRecult);
@@ -37,11 +65,10 @@ public class ListRecipeActivity extends Activity{
 			JSONArray jsonArray = jsonObject.getJSONArray("root");
 			//System.out.println(jsonArray);
 			//testView.append(jsonArray.toString());
-			ArrayList<String> list = new ArrayList<String>();
+			ArrayList<JSONObject> list = new ArrayList<JSONObject>();
 			for(int i=0;i<jsonArray.length();i++){   
                 JSONObject jo = (JSONObject)jsonArray.opt(i);
-                list.add(jo.toString());
-                //ss[i]=jo.getInt("id")+"  "+jo.getString("name")+"  "+jo.getString("地址");  
+                list.add(jo);
             }
 			
 			 //(ArrayList)JSONArray.toList(jsonArray, String.class);
@@ -93,19 +120,117 @@ public class ListRecipeActivity extends Activity{
         return result;
 	}
 	
-	/*public String[] analyticJson(String resultJson){
-		try {  
-            String[] ss=new String[5]; 
-            JSONObject jsonObject = new JSONObject(resultJson).getJSONObject("root");  
-            JSONArray jsonArray=jsonObject.getJSONArray("info");  
-            for(int i=0;i<jsonArray.length();i++){   
-                JSONObject jo = (JSONObject)jsonArray.opt(i);  
-                ss[i]=jo.getInt("id")+"  "+jo.getString("name")+"  "+jo.getString("地址");  
-            }
-            return ss;  
-        } catch (JSONException e) {  
-            e.printStackTrace();  
-        }  
-        return null;  
-	}*/
+	class ListRecipeAdapter extends BaseAdapter{
+
+		ArrayList<JSONObject> list;
+		Context context;
+		private Activity activity;  
+	    //private ArrayList<HashMap<String, String>> data;  
+	    private LayoutInflater inflater=null;  
+	    public ImageLoader imageLoader;
+	    
+	    public ListRecipeAdapter(Activity a, ArrayList<JSONObject> d){
+	    	activity = a;  
+	        list=d;
+	        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	        //imageLoader=new ImageLoader();
+	        //imageLoader=new ImageLoader(activity.getApplicationContext());
+	    }
+	    
+		public void setContext(Context context){
+			this.context = context;
+		}
+
+		public void setArrayList(ArrayList<JSONObject> list){
+			this.list = list;
+		}
+		
+		public int getCount(){
+			return list.size();
+		}
+		
+		public JSONObject getItem(int positon){
+			JSONObject item = list.get(positon);
+			return item;
+		}
+		
+		public long getItemId(int position){
+			return position;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent){
+			/*TextView textView = new TextView(context);
+			textView.setTextSize(1,30);
+			String string = getItem(position);
+			textView.setText(string);
+			return textView;*/
+			
+			View vi=convertView;
+	        if(convertView==null)
+	            vi = inflater.inflate(R.layout.activity_recipe_list_all_inner, null);
+	  
+	        TextView name = (TextView)vi.findViewById(R.id.recipe_name); 
+	        TextView material = (TextView)vi.findViewById(R.id.recipe_material); 
+	        TextView hot = (TextView)vi.findViewById(R.id.recipe_hot);
+	        ImageView thumb_image=(ImageView)vi.findViewById(R.id.list_image); // 缩略图  
+	          
+	        //HashMap<String, String> song = new HashMap<String, String>();  
+	        //item = list.get(position);
+	          
+	        // 设置ListView的相关值  
+	        name.setText("recipe name here");
+	        try {
+	        	name.setText(list.get(position).getString("recipeName"));
+	        	String mString = "";
+	        	JSONArray mArray = list.get(position).getJSONArray("material");
+	        	for(int i=0;i<mArray.length();i++){
+	                JSONObject jo = (JSONObject)mArray.opt(i);
+	                mString +=jo.getString("materialName")+"  ";
+	            }
+				material.setText(mString);
+				hot.setText("Collect Num : " + list.get(position).getString("collectNum")+"    Comment Num : "+list.get(position).getString("commentNum"));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+	        //hot.setText(song.get(CustomizedListView.KEY_DURATION));
+	        
+	        try {
+	        	File cacheDir = StorageUtils.getOwnCacheDirectory(getApplicationContext(), "imageloader/Cache");
+	        	
+	        	ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())  
+                .threadPriority(Thread.NORM_PRIORITY - 2)  
+                .denyCacheImageMultipleSizesInMemory()  
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())  
+                .tasksProcessingOrder(QueueProcessingType.LIFO)  
+                //.enableLogging() // Not necessary in common  
+                .build();  
+	        	ImageLoader.getInstance().init(config);
+	        	
+				ImageLoader.getInstance()
+				.displayImage("http://123.57.38.31:3000/"+list.get(position).getString("photo").trim(), thumb_image, options, new SimpleImageLoadingListener() {
+					/*@Override
+					public void onLoadingStarted(String imageUri, View view) {
+						holder.progressBar.setProgress(0);
+						holder.progressBar.setVisibility(View.VISIBLE);
+					}
+
+					@Override
+					public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+						holder.progressBar.setVisibility(View.GONE);
+					}
+
+					@Override
+					public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+						holder.progressBar.setVisibility(View.GONE);
+					}*/
+				});
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        //imageLoader.DisplayImage(list.get(position).getString("photo"), thumb_image);
+	        return vi;
+		}
+	}
 }
