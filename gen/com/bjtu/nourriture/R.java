208421815e26abb070691678af305b1e8e399bd0
mengchi6131 <mengchi6131@gmail.com>
2015-01-09 15:59:51 +0800

    rmc

--- gen/com/bjtu/nourriture/R.java
+++ gen/com/bjtu/nourriture/R.java
@@ -32,66 +32,70 @@ containing a value of this type.
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int img_1=0x7f020001;
-        public static final int img_2=0x7f020002;
-        public static final int img_3=0x7f020003;
-        public static final int img_4=0x7f020004;
-        public static final int img_5=0x7f020005;
-        public static final int img_frame_background=0x7f020006;
+        public static final int corners_bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int img_1=0x7f020002;
+        public static final int img_2=0x7f020003;
+        public static final int img_3=0x7f020004;
+        public static final int img_4=0x7f020005;
+        public static final int img_5=0x7f020006;
+        public static final int img_frame_background=0x7f020007;
+        public static final int like1=0x7f020008;
+        public static final int like2=0x7f020009;
+        public static final int pro_zan_sel=0x7f02000a;
+        public static final int pro_zan_unsel=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080020;
+        public static final int action_settings=0x7f080031;
+        public static final int author=0x7f080025;
         public static final int button1=0x7f080005;
         public static final int button2=0x7f080006;
-        public static final int five=0x7f08001f;
-        public static final int four=0x7f08001e;
+        public static final int content=0x7f080020;
+        public static final int five=0x7f080030;
+        public static final int four=0x7f08002f;
+        public static final int gridView1=0x7f080022;
         public static final int id_menu=0x7f080003;
-<<<<<<< HEAD
-        public static final int imageView2=0x7f08000d;
-        public static final int imageView3=0x7f08000e;
-        public static final int imageView4=0x7f080010;
-        public static final int imageView5=0x7f080012;
-        public static final int imageView6=0x7f080014;
+        public static final int imageView5=0x7f080016;
         public static final int img_address=0x7f080001;
         public static final int img_friends=0x7f080002;
         public static final int img_weixin=0x7f080000;
-        public static final int listView1=0x7f080006;
-        public static final int list_image=0x7f080008;
-        public static final int one=0x7f080017;
-        public static final int recipe_hot=0x7f08000b;
-        public static final int recipe_material=0x7f08000a;
-        public static final int recipe_name=0x7f080009;
-        public static final int singleRecipePhoto=0x7f08000c;
-=======
-        public static final int imageView1=0x7f08000d;
-        public static final int imageView2=0x7f08000e;
-        public static final int imageView3=0x7f08000f;
-        public static final int imageView4=0x7f080011;
-        public static final int imageView5=0x7f080013;
-        public static final int imageView6=0x7f080015;
-        public static final int img_address=0x7f080001;
-        public static final int img_friends=0x7f080002;
-        public static final int img_weixin=0x7f080000;
+        public static final int likeCount=0x7f080027;
+        public static final int likeView=0x7f080026;
+        public static final int linearLayout1=0x7f08001e;
         public static final int listView1=0x7f080007;
         public static final int list_image=0x7f080009;
-        public static final int one=0x7f08001b;
+        public static final int name=0x7f08001f;
+        public static final int one=0x7f08002c;
+        public static final int picture=0x7f080023;
         public static final int recipe_hot=0x7f08000c;
         public static final int recipe_material=0x7f08000b;
         public static final int recipe_name=0x7f08000a;
->>>>>>> d98c7ba70d86c44530e2d305a2b0942b3ba7b1c8
+        public static final int singleAuthorInfo=0x7f08000f;
+        public static final int singleCollectNumPic=0x7f080012;
+        public static final int singleCommentNumPic=0x7f080014;
+        public static final int singleRecipeAuthor=0x7f080011;
+        public static final int singleRecipeAuthorHead=0x7f080010;
+        public static final int singleRecipeCollectNum=0x7f080013;
+        public static final int singleRecipeCommentNum=0x7f080015;
+        public static final int singleRecipeDescription=0x7f080018;
+        public static final int singleRecipeDifficultity=0x7f08001b;
+        public static final int singleRecipeName=0x7f08000e;
+        public static final int singleRecipePhoto=0x7f08000d;
+        public static final int singleRecipeTime=0x7f08001c;
+        public static final int surfaceView1=0x7f080019;
+        public static final int surfaceView2=0x7f08001d;
+        public static final int tableRow1=0x7f08001a;
         public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080010;
-        public static final int textView3=0x7f080012;
-        public static final int textView4=0x7f080014;
-        public static final int textView5=0x7f080016;
-        public static final int three=0x7f08001d;
+        public static final int textView4=0x7f080017;
+        public static final int three=0x7f08002e;
         public static final int thumbnail=0x7f080008;
-        public static final int topic_list_view=0x7f080017;
-        public static final int topic_name=0x7f080018;
-        public static final int topic_short_content=0x7f080019;
-        public static final int topic_upload=0x7f08001a;
-        public static final int two=0x7f08001c;
+        public static final int topic_list_view=0x7f080028;
+        public static final int topic_name=0x7f080029;
+        public static final int topic_short_content=0x7f08002a;
+        public static final int topic_upload=0x7f08002b;
+        public static final int two=0x7f08002d;
+        public static final int upload_name=0x7f080024;
+        public static final int uploadcount=0x7f080021;
     }
     public static final class layout {
         public static final int activity_bottom=0x7f030000;
@@ -100,9 +104,10 @@ containing a value of this type.
         public static final int activity_recipe_list_all_inner=0x7f030003;
         public static final int activity_recipe_single=0x7f030004;
         public static final int activity_topic_detail=0x7f030005;
-        public static final int activity_topic_list_all=0x7f030006;
-        public static final int activity_topic_list_all_inner=0x7f030007;
-        public static final int layout_menu=0x7f030008;
+        public static final int activity_topic_detail_inner=0x7f030006;
+        public static final int activity_topic_list_all=0x7f030007;
+        public static final int activity_topic_list_all_inner=0x7f030008;
+        public static final int layout_menu=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -112,6 +117,14 @@ containing a value of this type.
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int recipe_collectRecipe=0x7f050008;
+        public static final int recipe_cookTime=0x7f05000a;
+        public static final int recipe_difficultity=0x7f050009;
+        public static final int recipe_material=0x7f05000b;
+        public static final int recipe_recipeAuthor=0x7f050005;
+        public static final int recipe_recipeCollectNum=0x7f050007;
+        public static final int recipe_recipeCommentNum=0x7f050006;
+        public static final int recipe_recipeName=0x7f050004;
         public static final int title_activity_topic_detail=0x7f050003;
     }
     public static final class style {
