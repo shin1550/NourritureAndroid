7c84f45776c6cff8b5eebef9e169472cfbd9ed82
MengJaeSuk <MengJaeSul@gmail.com>
2015-01-09 22:41:43 +0800

    01.09 收工

--- src/com/bjtu/nourriture/common/Constants.java
+++ src/com/bjtu/nourriture/common/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
 	public static final String INTENT_EXTRA_SINGLE_RECIPE_ID = "singleRecipeId";
 	
 	public static final String POST_RECIPE_COMMENT_CONENT = "content";
-	public static final String POST_RECIPE_COMMENT_REPLYID = "replayId";
+	public static final String POST_RECIPE_COMMENT_REPLYID = "replyId";
 }
