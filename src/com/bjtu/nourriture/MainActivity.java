994228bd91a5c0d9f2ab6663fd81f3e5d89bac7a
liuhanxu <liuhanxu2013@gmail.com>
2015-01-09 13:04:36 +0800

    lhx   1.简单版登录，存了本地的session，后续会更改

--- src/com/bjtu/nourriture/MainActivity.java
+++ src/com/bjtu/nourriture/MainActivity.java
@@ -3,21 +3,42 @@ package com.bjtu.nourriture;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.bjtu.nourriture.common.Session;
 import com.bjtu.nourriture.recipe.ListRecipeActivity;
 import com.bjtu.nourriture.topic.ListTopicActivity;
-//hihihi
-//da ping guo
+import com.bjtu.nourriture.user.LoginActivity;
+
 public class MainActivity extends Activity {
 
 	private SlidingMenu mMenu;
+	private View layout_1,layout_1_1;
+	private TextView menu_1_1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		layout_1 = this.findViewById(R.id.layout_1);
+		layout_1_1 = this.findViewById(R.id.layout_1_1);
+		menu_1_1 = (TextView)this.findViewById(R.id.menu_1_1);
+		//逻辑还存在问题，待修改
+		Session session = Session.getSession();
+		Boolean islogin=(Boolean)session.get("islogin");
+		System.out.println("islogin----"+islogin);
+		if(islogin!=null){
+			if(islogin){
+				layout_1.setVisibility(View.GONE);
+				layout_1_1.setVisibility(View.VISIBLE);
+				String username = (String) session.get("username");
+				String head = (String) session.get("head");
+				menu_1_1.setText(username);
+			}
+		}
 		mMenu = (SlidingMenu) findViewById(R.id.id_menu);
 	}
 
@@ -48,6 +69,13 @@ public class MainActivity extends Activity {
 	public void toListTopic(View view){
 		Intent intent = new Intent();
 		intent.setClass(MainActivity.this,ListTopicActivity.class);
+		startActivity(intent);
+	}
+	
+	public void toMenu1(View view){
+		System.out.println("1");
+		Intent intent = new Intent();
+		intent.setClass(MainActivity.this,LoginActivity.class);
 		startActivity(intent);
 	}
 	
---
+++ src/com/bjtu/nourriture/user/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.bjtu.nourriture.user;
+
+import com.bjtu.nourriture.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+	}
+
+
+}
