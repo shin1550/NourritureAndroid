f7883bc508aa2c1f9d2098f3bdbe322ae9e9754a
mengchi6131 <mengchi6131@gmail.com>
2015-01-09 16:13:36 +0800

    rmc

--- src/com/bjtu/nourriture/common/Constants.java
+++ src/com/bjtu/nourriture/common/Constants.java
@@ -4,9 +4,6 @@ public class Constants {
 
 	//from listRecipe to singleRecipe intent's extra variable
 	public static final String INTENT_EXTRA_SINGLE_RECIPE = "singleRecipe";
-<<<<<<< HEAD
 	public static final String INTENT_EXTRA_TOPIC_DETAIL = "topicDetail";
-=======
 	public static final String INTENT_EXTRA_SINGLE_RECIPE_ID = "singleRecipeId";
->>>>>>> 665bb41193233bd09d0028069dd34700e02723c4
 }
